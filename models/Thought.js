d745c81a1a6a1521d637070bfc44d4d2174c7da5
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-04-02 18:27:30 -0400

    consistent dateFormat

--- models/Thought.js
+++ models/Thought.js
@@ -41,13 +41,7 @@ const ThoughtSchema = new Schema({
       createdAt: {
         type: Date,
         default: Date.now,
-        get: (timestamp) => {
-          const date = new Date(timestamp);
-          const month = date.getMonth() + 1;
-          const day = date.getDate();
-          const year = date.getFullYear();
-          return `${month}/${day}/${year}`;
-        },
+        get: dateFormat,
       },
       username: {
         type: String,
